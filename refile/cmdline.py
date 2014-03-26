bcd257eb8c50e17a94ce4a381022bd469f5fda75
Sean Marshallsay <srm.1708@gmail.com>
2014-03-26 21:57:23 +0000

    Improve argument parser.
    Add more arguments to the parser. Add a class which my be used to run the main
    program.

--- refile/cmdline.py
+++ refile/cmdline.py
@@ -2,17 +2,29 @@ import sys
 import argparse
 
 
+class Engine:
+    pass
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description="""Interact with files whose names match the regular
+        description='''Interact with files whose names match the regular
         expression PATTERN. See `pydoc refile` for comprehensive
-        documentation."""
+        documentation.'''
     )
 
-    parser.add_argument("PATTERN", help="regex to match filenames against")
+    parser.add_argument('PATTERN', help='regex to match filenames against')
+    parser.add_argument(
+        'DIR',
+        nargs='?',
+        default='.',
+        help='directory to search for files (defaults to current directory)'
+    )
 
-    args = parser.parse_args()
+    matcher = parser.parse_args(namespace=Engine())
+
+    print(vars(matcher))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
